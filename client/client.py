485f152ac2ae751d9cf278bf93a6a3e8386230a8
Reiyo <reiyo.oishi@g.softbank.co.jp>
2025-06-05 07:53:31 +0000

    Prepare client and host

---
+++ client/client.py
@@ -0,0 +1,118 @@
+import asyncio
+from contextlib import AsyncExitStack
+from typing import Any, Optional
+
+from mcp import ClientSession, StdioServerParameters
+from mcp.client.sse import sse_client
+from mcp.client.stdio import stdio_client
+from mcp.client.streamable_http import streamablehttp_client
+from mcp.types import EmbeddedResource, ImageContent, TextContent
+
+
+class MCPClient:
+    def __init__(self, server_parameters: dict[str, Any]):
+        self.server_parameters = server_parameters
+        self.session: Optional[ClientSession] = None
+
+        self.exit_stack = AsyncExitStack()
+
+    async def _connect_stdio_server(self):
+        if ("command" not in self.server_parameters or
+                "args" not in self.server_parameters):
+            raise ValueError("Server parameters must include 'command' and 'args'")
+
+        server_params = StdioServerParameters(
+            command=self.server_parameters.get("command"),
+            args=self.server_parameters.get("args"),
+            env=self.server_parameters.get("env", None),
+        )
+        stdio_transport = await self.exit_stack.enter_async_context(
+            stdio_client(server_params)
+        )
+        self.stdio, self.write = stdio_transport
+        self.session = await self.exit_stack.enter_async_context(
+            ClientSession(self.stdio, self.write)
+        )
+        await self.session.initialize()
+        return self
+
+    async def _connect_sse_server(self):
+        if "url" not in self.server_parameters:
+            raise ValueError("Server parameters must include 'url'")
+
+        url = self.server_parameters.get("url")
+        sse_transport = await self.exit_stack.enter_async_context(sse_client(url=url))
+        self.stdio, self.write = sse_transport
+        self.session = await self.exit_stack.enter_async_context(
+            ClientSession(self.stdio, self.write)
+        )
+        await self.session.initialize()
+        return self
+
+    async def _connect_streamablehttp_server(self):
+        if "url" not in self.server_parameters:
+            raise ValueError("Server parameters must include 'url'")
+
+        url = self.server_parameters.get("url")
+        streamablehttp_transport = await self.exit_stack.enter_async_context(
+            streamablehttp_client(url=url)
+        )
+        self.stdio, self.write, _ = streamablehttp_transport
+        self.session = await self.exit_stack.enter_async_context(
+            ClientSession(self.stdio, self.write)
+        )
+        await self.session.initialize()
+        return self
+
+
+    async def __aenter__(self):
+        transport = self.server_parameters.get("transport", "stdio")
+        if transport == "stdio":
+            return await self._connect_stdio_server()
+        elif transport == "sse":
+            return await self._connect_sse_server()
+        elif transport == "streamable_http":
+            return await self._connect_streamablehttp_server()
+        else:
+            raise ValueError(f"Unknown transport type: {transport}")
+
+    async def __aexit__(self, exc_type, exc, tb):
+        await self.exit_stack.aclose()
+
+    async def _get_tools(self):
+        list_tools_response = await self.session.list_tools()
+        # OpenAI tool format is only taken into account
+        return [
+            {
+                "type": "function",
+                "function": {
+                    "name": tool.name,
+                    "description": tool.description,
+                    "parameters": tool.inputSchema,
+                },
+            }
+            for tool in list_tools_response.tools
+        ]
+
+    def _encode_tool_content(self, content: TextContent | ImageContent | EmbeddedResource) -> dict[str, Any]:
+        if isinstance(content, TextContent):
+            return {"type": "text", "text": content.text}
+        elif isinstance(content, ImageContent):
+            return {"type": "image_url", "image_url": {"url": content.url}}
+        else:
+            raise ValueError(f"Unsupported content type: {type(content)}")
+
+    async def execute_tool(self, name: str, arguments: dict[str, Any]):
+        result = await self.session.call_tool(name, arguments)
+        return [self._encode_tool_content(c) for c in result.content]
+
+async def main(server_parameters: dict[str, Any]):
+    async with MCPClient(server_parameters) as client:
+        await client.execute_tool(name="get_softreef_component_file_path", arguments={"component": "TextBox"})
+
+if __name__ == "__main__":
+    server_parameters = {
+        "transport": "streamable_http",
+        "url": "http://localhost:9999",
+    }
+    asyncio.run(main(server_parameters))
---
+++ host/utils.py
@@ -0,0 +1,38 @@
+import sys
+
+
+class Color:
+    GRAY = "\033[90m"
+    RED = "\033[91m"
+    GREEN = "\033[92m"
+    YELLOW = "\033[93m"
+    BLUE = "\033[94m"
+    PURPLE = "\033[95m"
+    CYAN = "\033[96m"
+    WHITE = "\033[97m"
+    RETURN = "\033[07m"  # 反転
+    ACCENT = "\033[01m"  # 強調
+    FLASH = "\033[05m"  # 点滅
+    RED_FLASH = "\033[05;41m"  # 赤背景+点滅
+    END = "\033[0m"
+
+def user_input(str: str):
+    try:
+        query = input(Color.GREEN + str).rstrip()
+        print(Color.END)
+        return query
+    except KeyboardInterrupt:
+        print(Color.RED + "\n入力が中断されました" + Color.END)
+        sys.exit(1)
+
+
+def llm_print(str: str):
+    print(Color.BLUE + str + Color.END)
+
+
+def event_print(str: str):
+    print(Color.GRAY + str + Color.END)
+
+
+def error_print(str: str):
+    print(Color.RED + str + Color.END)
